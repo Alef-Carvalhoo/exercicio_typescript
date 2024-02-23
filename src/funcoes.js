8bdcd2013ae60118a85c15b8f53850f11debe2cd
Alef Carvalho <alefcarvalho200310@gmail.com>
2024-02-23 16:11:39 -0300

    commit inicial

---
+++ src/funcoes.js
@@ -0,0 +1,8 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome = "Alef") {
+    return `${nome} Diz Olá`;
+}
+console.log(saudacao);
---
+++ src/funcoes.ts
@@ -0,0 +1,9 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2 ;
+}
+
+function saudacao(nome: string = "Alef"): string {
+    return `${nome} Diz Olá`
+}
+
+console.log(saudacao)
